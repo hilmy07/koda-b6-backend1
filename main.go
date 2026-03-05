e06c2132c2800bbaad02c3648a4bab4721146cf1
mrrcodex <mrramadhandev@gmail.com>
2026-03-05 09:23:51 +0700

    feat: add swaggo for documentation

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @title             Backend Apps
+// @version           1.0.0
+// @description       This is basic backend apps
+// @host              localhost:8000
+// @BasePath          /
+
 func main() {
 	r := gin.Default()
 
